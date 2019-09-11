5110263abdb7d2662b41c12232ccbf31e91c61e8
efeldcy <55018112+efeldcy@users.noreply.github.com>
2019-09-11 13:48:58 +0200

    Update 0.3-3

--- Held.java
+++ Held.java
@@ -12,79 +12,121 @@ Herocy HC;
 // Konstruktor
 public Held(int pSpielernummer,String pName)
 {
-Spielernummer=pSpielernummer;
-Name=pName;
+        Spielernummer=pSpielernummer;
+        Name=pName;
 //HC=pHC;
-setImage("Held"+Spielernummer+".png"); // Für jeden Helden ein eignes Bild!
-                                        // z.B. 'Held3.png' für den 3. Helden
+        setImage("Held"+Spielernummer+".png"); // Für jeden Helden ein eignes Bild!
+                                               // z.B. 'Held3.png' für den 3. Helden
 } // Konstuktor
 
 public void act(){
-  KeyListener();
+        KeyListener(true);
 }
 
-public void KeyListener(){
-    if(Greenfoot.isKeyDown("a")){
-        Greenfoot.delay(2); //Zwei Zeiteinheiten delay
+public void KeyListener(boolean debug){
 
-      //Movement LINKS
-      if(!HC.Spielfeld[getX()][getY()].equals("X")){
-        move(-1);
-      }
-      //Movement LINKS RUNTER
-      if(!HC.Spielfeld[getX()+1][getY()+1].equals("X") && !HC.Spielfeld[getX()+1][getY()+1].equals("L")){
-        setLocation(getX(), getY()+1);
-      }
-    }
+        if(debug) {
+                System.out.println("X: "+getX()+" | Y: "+getY());
+        }
 
-    if(Greenfoot.isKeyDown("d")){
-        Greenfoot.delay(2); //Zwei Zeiteinheiten delay
 
-      //Movement RECHTS
-      if(!HC.Spielfeld[getX()+2][getY()].equals("X")){
-        move(1);
-      }
-      //Movement RECHTS RUNTER
-      if(!HC.Spielfeld[getX()+1][getY()+1].equals("X") && !HC.Spielfeld[getX()+1][getY()+1].equals("L")){
-        setLocation(getX(), getY()+1);
-      }
-    }
+        if(Greenfoot.isKeyDown("a") && !isAtEdge("left") && !isOnLadder()) {
+                Greenfoot.delay(2); //Zwei Zeiteinheiten delay
 
-    if(Greenfoot.isKeyDown("space")){
-        Greenfoot.delay(2); //Zwei Zeiteinheiten delay
+                //Movement LINKS
+                if(!HC.Spielfeld[getX()][getY()].equals("X")) {
+                        move(-1);
+                }
+                //Movement LINKS RUNTER
+                if(!HC.Spielfeld[getX()+1][getY()+1].equals("X") && !HC.Spielfeld[getX()+1][getY()+1].equals("L")) {
+                        setLocation(getX(), getY()+1);
+                }
+        }
 
-      //Movement SPRUNG RECHTS
-      if(HC.Spielfeld[getX()+2][getY()].equals("X")){
-        setLocation(getX()+1, getY()-1);
-      }
-      //Movement SPRUNG LINKS
-      if(HC.Spielfeld[getX()][getY()].equals("X")){
-        setLocation(getX()-1, getY()-1);
-      }
-    }
+        if(Greenfoot.isKeyDown("d") && !isAtEdge("right") && !isOnLadder()) {
+                Greenfoot.delay(2); //Zwei Zeiteinheiten delay
 
-    if(Greenfoot.isKeyDown("shift")){
-        Greenfoot.delay(2); //Zwei Zeiteinheiten delay
+                //Movement RECHTS
+                if(!HC.Spielfeld[getX()+2][getY()].equals("X")) {
+                        move(1);
+                }
+                //Movement RECHTS RUNTER
+                if(!HC.Spielfeld[getX()+1][getY()+1].equals("X") && !HC.Spielfeld[getX()+1][getY()+1].equals("L")) {
+                        setLocation(getX(), getY()+1);
+                }
+        }
 
-      //Movement HOCH KLETTERN
-      if(HC.Spielfeld[getX()+1][getY()].equals("L")){
-        setLocation(getX(), getY()-1);
-      }
-    }
+        if(Greenfoot.isKeyDown("space") && !isAtEdge("any")) {
+                Greenfoot.delay(2); //Zwei Zeiteinheiten delay
 
-    if(Greenfoot.isKeyDown("control")){
-        Greenfoot.delay(2); //Zwei Zeiteinheiten delay
+                //Movement SPRUNG RECHTS
+                if(HC.Spielfeld[getX()+2][getY()].equals("X")) {
+                        setLocation(getX()+1, getY()-1);
+                }
+                //Movement SPRUNG LINKS
+                if(HC.Spielfeld[getX()][getY()].equals("X")) {
+                        setLocation(getX()-1, getY()-1);
+                }
+        }
 
-      //Movement RUNTER KLETTERN
-      if(HC.Spielfeld[getX()+1][getY()+1].equals("L")){
-        setLocation(getX(), getY()+1);
-      }
-    }
-  }
+        if(Greenfoot.isKeyDown("shift")) {
+                Greenfoot.delay(2); //Zwei Zeiteinheiten delay
+
+                //Movement HOCH KLETTERN
+                if(HC.Spielfeld[getX()+1][getY()].equals("L")) {
+                        setLocation(getX(), getY()-1);
+                }
+        }
+
+        if(Greenfoot.isKeyDown("control")) {
+                Greenfoot.delay(2); //Zwei Zeiteinheiten delay
+
+                //Movement RUNTER KLETTERN
+                if(HC.Spielfeld[getX()+1][getY()+1].equals("L")) {
+                        setLocation(getX(), getY()+1);
+                }
+        }
+}
+
+public boolean isOnLadder(){
+        if(getX() == 16 && getY() < 7 && getY() > 2) {
+                return true;
+        }
+        else{
+                return false;
+        }
+}
+
+public boolean isAtEdge(String location){
+        switch(location) {
+        case "right":
+                if(getX() == 19) {
+                        return true;
+                }
+                break;
+
+        case "left":
+                if(getX() == 0) {
+                        return true;
+                }
+                break;
+
+        case "any":
+                if(getX() == 19) {
+                        return true;
+                }
+                if(getX() == 0) {
+                        return true;
+                }
+                break;
+
+        }
+        return false;
+}
 
 public void addedToWorld(World pW)
 {
-HC=(Herocy) pW;
+        HC=(Herocy) pW;
 } // addedToWorld
 
 } // class Held
--- Herocy.java
+++ Herocy.java
@@ -6,68 +6,69 @@ String[][] Spielfeld=new String[21][11];
 String[] Zeile=new String[11];
 int anzahlHelden=3;
 Held Held1,Held2,Held3;
-Heiler H;Fee F;Markt M;Kiosk K;
+Heiler H; Fee F; Markt M; Kiosk K;
+
 public Herocy()
 {
-super(20,11,40); // Spielfeld 20x10 Felder; Jedes Feld 40x40 Pixel
-Zeile[1]= "                   F";
-Zeile[2]= "                    ";
-Zeile[3]= "XXXXXXXXXXXXXXXXLXXX";
-Zeile[4]= "                L   ";
-Zeile[5]= "                L   ";
-Zeile[6]= "                L   ";
-Zeile[7]= "                L HM";
-Zeile[8]= "             XXXXXXX";
-Zeile[9]= "X         XXXXXXXXXX";
-Zeile[10]="XXXXXXXXXXXXXXXXXXXX";
+        super(20,11,40); // Spielfeld 20x10 Felder; Jedes Feld 40x40 Pixel
+        Zeile[1]= "                   F";
+        Zeile[2]= "                    ";
+        Zeile[3]= "XXXXXXXXXXXXXXXXLXXX";
+        Zeile[4]= "                L   ";
+        Zeile[5]= "                L   ";
+        Zeile[6]= "                L   ";
+        Zeile[7]= "                L H ";
+        Zeile[8]= "             XXXXXXX";
+        Zeile[9]= "          XXXXXXXXXX";
+        Zeile[10]="XXXXXXXXXXXXXXXXXXXX";
 
-for(int i=1;i<=10;i++)
-{
-    for(int a=0;a<20;a++)
-    {
-        String Symbol=Zeile[i].substring(a,a+1);
-        Spielfeld[a+1][i]=Symbol;
-        if(Symbol.equals("X"))
+        for(int i=1; i<=10; i++)
         {
-            Hindernis H=new Hindernis();
-            addObject(H,a,i);
-        } // if
-        if(Symbol.equals("L"))
-        {
-            Leiter L=new Leiter();
-            addObject(L,a,i);
-        } // if
-        if(Symbol.equals("F"))
-        {
-            F=new Fee();
-            addObject(F,a,i);
-        } // if
-        if(Symbol.equals("M"))
-        {
-            M=new Markt();
-            addObject(M,a,i);
-        } // if
-        if(Symbol.equals("H"))
-        {
-            H=new Heiler();
-            addObject(H,a,i);
-        } // if
-        if(Zeile[i].substring(a,a+1).equals("K"))
-        {
-            K=new Kiosk();
-            addObject(K,a,i);
-        } // if
-    } // for a
-} // for i
-HeldenInitialisieren();
+                for(int a=0; a<20; a++)
+                {
+                        String Symbol=Zeile[i].substring(a,a+1);
+                        Spielfeld[a+1][i]=Symbol;
+                        if(Symbol.equals("X"))
+                        {
+                                Hindernis H=new Hindernis();
+                                addObject(H,a,i);
+                        } // if
+                        if(Symbol.equals("L"))
+                        {
+                                Leiter L=new Leiter();
+                                addObject(L,a,i);
+                        } // if
+                        if(Symbol.equals("F"))
+                        {
+                                F=new Fee();
+                                addObject(F,a,i);
+                        } // if
+                        if(Symbol.equals("M"))
+                        {
+                                M=new Markt();
+                                addObject(M,a,i);
+                        } // if
+                        if(Symbol.equals("H"))
+                        {
+                                H=new Heiler();
+                                addObject(H,a,i);
+                        } // if
+                        if(Zeile[i].substring(a,a+1).equals("K"))
+                        {
+                                K=new Kiosk();
+                                addObject(K,a,i);
+                        } // if
+                } // for a
+        } // for i
+        HeldenInitialisieren();
 //Greenfoot.playSound("music.wav");
-super.setBackground("background.png");
+        super.setBackground("background.png");
 
 } // Konstruktor
 public void HeldenInitialisieren()
 {
-Held1=new Held(1,"Herocy");
-addObject(Held1,3,9);
+        Held1=new Held(1,"Herocy");
+        addObject(Held1,3,9);
 //Held2=new Held(2,"cyGuy");
 //addObject(Held2,1,9);
 } // HeldenInitialisiere
